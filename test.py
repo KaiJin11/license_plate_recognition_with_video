44a9037c9e0d904cabc2f2018d877b0fed530dbb
kai <kaij5088@gmail.com>
2021-01-05 16:26:55 -0800

    modify test.py
    modify test.py by adding time_break

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from plate_detector import Plate_Detector
 pdetect_obj = Plate_Detector()
-
+time_break = 5
 video_path = 'video/ALRP_left_zone_A_wo_ir_dots.avi' #1500
 output_path = 'data_output_2'
 result_plates = pdetect_obj.video_scaner(video_path, output_path, time_break = time_break, number_frame = 1500)
